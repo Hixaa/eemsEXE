fbafc02a9a527025844511e377907bd363370f39
Tanish pophale <tanishpophale@gmail.com>
2024-06-08 16:27:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,6 +45,7 @@ pip3 install chardet
 #sudo chmod +x /cleanup.sh
 sudo mv RakshaIOT /
 sudo chmod 777 /RakshaIOT
+sudo mv config_xaees.ini /
 sudo mv xaees.service /lib/systemd/system/
 #sudo cp -r xaees/* /
 
